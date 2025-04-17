f4a20ad166845d2c0155be323911c7f2e847b579
Théo <t.knopfer@gmail.com>
2025-04-17 14:26:27 +0200

    JS journal

---
+++ script.js
@@ -0,0 +1,35 @@
+//Carroussel
+
+//exercice interactif
+
+//journal
+const form = document.querySelector('.input-container form');
+const input = document.querySelector('.input');
+const pendingList = document.getElementById('pendingList');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const entryText = input.value.trim();
+
+   if(entryText !== '') {
+    const existingEntries = Array.from(pendingList.querySelectorAll('.entries'));
+    const isDuplicate = existingEntries.some(entry => entry.textContent === entryText);
+
+    if (isDuplicate) {
+        alert('Cette entrée existe déjà.');
+        return;
+    }
+
+    if(existingEntries.length === 1 && existingEntries[0].classList.contains('example')) {
+        existingEntries[0].textContent = entryText;
+        existingEntries[0].classList.remove('example');
+    } else {
+        const newEntry = document.createElement('li');
+        newEntry.className = 'entries';
+        newEntry.textContent = entryText;
+        pendingList.appendChild(newEntry);
+    }
+
+    input.value = '';
+}
+  });
